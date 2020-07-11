3e25354807c567727cdac30b7f97e84a8da0585a
lloydmeta <lloydmeta@gmail.com>
2020-07-11 14:42:38 +0900

    Moar debug
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- schemafy_lib/src/lib.rs
+++ schemafy_lib/src/lib.rs
@@ -522,7 +522,9 @@ impl<'r> Expander<'r> {
     }
 
     fn expand_definitions(&mut self, schema: Rc<Schema>) {
+        println!("inside expand_definitions");
         for (name, def) in &schema.definitions {
+            println!("Expanding definition of [{}]", name);
             let type_decl = self.expand_schema(name, Rc::clone(def));
             let definition_tokens = match def.description {
                 Some(ref comment) => {
@@ -539,7 +541,9 @@ impl<'r> Expander<'r> {
     }
 
     fn expand_schema(&mut self, original_name: &str, schema: Rc<Schema>) -> TokenStream {
+        println!("Expanding definitions, original_name [{}]", original_name);
         self.expand_definitions(Rc::clone(&schema));
+        println("Done expanding definitions, original_name [{}]", original_name);
 
         let pascal_case_name = replace_invalid_identifier_chars(&original_name.to_pascal_case());
         self.current_type.clone_from(&pascal_case_name);
